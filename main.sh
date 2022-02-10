59bd144a8cfb614b032e9b678762de1b107f664a
2ps <pshingavi@gmail.com>
2022-02-10 01:01:43 -0600

    [OOPS]  Added in updates to main.sh to accommodate usage of internal Cleaner

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CLASSPATH=$CLASSPATH:./jdbc_unload-1.0_full.jar
+export _JAVA_OPTIONS=--add-opens=java.base/jdk.internal.ref=ALL-UNNAMED
 java ewoks.jdbc_unload.OracleTableUnload $@
-
